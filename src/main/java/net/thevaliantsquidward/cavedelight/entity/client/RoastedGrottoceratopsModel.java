6276c44e3d3cdc07d9a37a45e63972db4b1ee1aa
TheValiantSquidward <thevaliantsquidward@gmail.com>
2025-03-26 19:02:18 -0400

    more fixes

--- src/main/java/net/thevaliantsquidward/cavedelight/entity/client/RoastedGrottoceratopsModel.java
+++ src/main/java/net/thevaliantsquidward/cavedelight/entity/client/RoastedGrottoceratopsModel.java
@@ -63,21 +63,20 @@ public class RoastedGrottoceratopsModel<T extends Entity> extends EntityModel<T>
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
 
-		PartDefinition grottoceratops = partdefinition.addOrReplaceChild("grottoceratops", CubeListBuilder.create().texOffs(0, 0).addBox(-26.0F, -10.0F, -26.0F, 52.0F, 3.0F, 73.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -7.0F, 14.0F, 3.1416F, 0.0F, 0.0F));
+		PartDefinition grottoceratops = partdefinition.addOrReplaceChild("grottoceratops", CubeListBuilder.create().texOffs(0, 0).addBox(-26.0F, -10.0F, -26.0F, 52.0F, 3.0F, 73.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -7.0F, -13.0F, 0.0F, 0.0F, -3.1416F));
 
 		PartDefinition grotto_body = grottoceratops.addOrReplaceChild("grotto_body", CubeListBuilder.create(), PartPose.offset(0.0F, -24.0F, 21.0F));
 
 		PartDefinition grotto_neck = grotto_body.addOrReplaceChild("grotto_neck", CubeListBuilder.create().texOffs(150, 76).addBox(-5.0F, -7.0F, -11.0F, 10.0F, 14.0F, 20.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 3.0F, -21.0F, 0.3491F, 0.0F, 0.0F));
 
-		PartDefinition grotto_left_front_leg = grotto_body.addOrReplaceChild("grotto_left_front_leg", CubeListBuilder.create().texOffs(177, 35).addBox(-3.0F, -4.0F, -4.5F, 6.0F, 25.0F, 9.0F, new CubeDeformation(0.0F))
-				.texOffs(177, 35).addBox(-3.0F, -4.0F, -4.5F, 6.0F, 25.0F, 9.0F, new CubeDeformation(0.0F))
+		PartDefinition grotto_left_front_leg = grotto_body.addOrReplaceChild("grotto_left_front_leg", CubeListBuilder.create().texOffs(223, 34).addBox(-3.0F, -4.0F, -4.5F, 6.0F, 25.0F, 9.0F, new CubeDeformation(0.0F))
 				.texOffs(60, 14).addBox(-3.0F, 16.0F, -7.5F, 3.0F, 3.0F, 3.0F, new CubeDeformation(0.0F))
 				.texOffs(20, 0).addBox(-3.0F, 19.0F, -7.5F, 3.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(14.0F, 6.0F, -17.5F, -1.3963F, 0.0F, 0.0F));
 
 		PartDefinition grotto_right_front_leg = grotto_body.addOrReplaceChild("grotto_right_front_leg", CubeListBuilder.create().texOffs(177, 35).mirror().addBox(-3.0F, -4.0F, -4.5F, 6.0F, 25.0F, 9.0F, new CubeDeformation(0.0F)).mirror(false)
 				.texOffs(177, 35).mirror().addBox(-3.0F, -4.0F, -4.5F, 6.0F, 25.0F, 9.0F, new CubeDeformation(0.0F)).mirror(false)
-				.texOffs(60, 14).mirror().addBox(0.0F, 16.0F, -7.5F, 3.0F, 3.0F, 3.0F, new CubeDeformation(0.0F)).mirror(false)
-				.texOffs(20, 0).mirror().addBox(0.0F, 19.0F, -7.5F, 3.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(-14.0F, 6.0F, -17.5F, -1.3963F, 0.0F, 0.0F));
+				.texOffs(236, 20).mirror().addBox(0.0F, 16.0F, -7.5F, 3.0F, 3.0F, 3.0F, new CubeDeformation(0.0F)).mirror(false)
+				.texOffs(237, 9).mirror().addBox(0.0F, 19.0F, -7.5F, 3.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(-14.0F, 6.0F, -17.5F, -1.3963F, 0.0F, 0.0F));
 
 		PartDefinition grotto_tail = grotto_body.addOrReplaceChild("grotto_tail", CubeListBuilder.create().texOffs(0, 36).addBox(-6.0F, -6.0F, -3.0F, 12.0F, 12.0F, 21.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -2.0F, 10.0F, -0.48F, 0.0F, 0.0F));
 
@@ -95,7 +94,7 @@ public class RoastedGrottoceratopsModel<T extends Entity> extends EntityModel<T>
 				.texOffs(45, 41).addBox(0.0F, -20.0F, -27.0F, 0.0F, 5.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -1.0F, 9.0F));
 
 		PartDefinition meat_3 = grotto_body.addOrReplaceChild("meat_3", CubeListBuilder.create().texOffs(0, 126).addBox(-11.0F, -15.0F, -28.0F, 22.0F, 30.0F, 7.0F, new CubeDeformation(0.0F))
-				.texOffs(0, 43).addBox(0.0F, -20.0F, -27.0F, 0.0F, 5.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -1.0F, 17.0F));
+				.texOffs(4, 204).addBox(0.0F, -20.0F, -27.0F, 0.0F, 5.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -1.0F, 17.0F));
 
 		PartDefinition meat_4 = grotto_body.addOrReplaceChild("meat_4", CubeListBuilder.create().texOffs(92, 113).addBox(-11.0F, -15.0F, -29.0F, 22.0F, 30.0F, 7.0F, new CubeDeformation(0.0F))
 				.texOffs(0, 43).addBox(0.0F, -20.0F, -27.0F, 0.0F, 5.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -1.0F, 25.0F));
@@ -108,10 +107,10 @@ public class RoastedGrottoceratopsModel<T extends Entity> extends EntityModel<T>
 		PartDefinition grotto_l_hind_leg_segment = grotto_left_hind_leg.addOrReplaceChild("grotto_l_hind_leg_segment", CubeListBuilder.create().texOffs(0, 0).addBox(-2.0F, -2.0F, -1.0F, 6.0F, 13.0F, 8.0F, new CubeDeformation(0.0F))
 				.texOffs(186, 180).addBox(-3.0F, 11.0F, -3.0F, 8.0F, 2.0F, 11.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(3.0F, 8.0F, -2.0F, 0.3927F, 0.0F, 0.4363F));
 
-		PartDefinition grotto_right_hind_leg = grottoceratops.addOrReplaceChild("grotto_right_hind_leg", CubeListBuilder.create().texOffs(104, 175).mirror().addBox(-9.0F, -6.0F, -6.0F, 9.0F, 15.0F, 12.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(-11.0F, -19.0F, 21.0F, 1.1345F, 0.0F, 0.0F));
+		PartDefinition grotto_right_hind_leg = grottoceratops.addOrReplaceChild("grotto_right_hind_leg", CubeListBuilder.create().texOffs(61, 211).mirror().addBox(-9.0F, -6.0F, -6.0F, 9.0F, 15.0F, 12.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(-11.0F, -19.0F, 21.0F, 1.1345F, 0.0F, 0.0F));
 
-		PartDefinition grotto_R_hind_leg_segment = grotto_right_hind_leg.addOrReplaceChild("grotto_R_hind_leg_segment", CubeListBuilder.create().texOffs(0, 0).mirror().addBox(-2.0F, 1.0F, -1.0F, 6.0F, 13.0F, 8.0F, new CubeDeformation(0.0F)).mirror(false)
-				.texOffs(186, 180).mirror().addBox(-3.0F, 14.0F, -3.0F, 8.0F, 2.0F, 11.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(-5.0F, 8.0F, -2.0F, 0.4363F, 0.0F, -0.4363F));
+		PartDefinition grotto_R_hind_leg_segment = grotto_right_hind_leg.addOrReplaceChild("grotto_R_hind_leg_segment", CubeListBuilder.create().texOffs(106, 217).mirror().addBox(-2.0F, 1.0F, -1.0F, 6.0F, 13.0F, 8.0F, new CubeDeformation(0.0F)).mirror(false)
+				.texOffs(136, 225).mirror().addBox(-3.0F, 14.0F, -3.0F, 8.0F, 2.0F, 11.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(-5.0F, 8.0F, -2.0F, 0.4363F, 0.0F, -0.4363F));
 
 		PartDefinition grotto_head = grottoceratops.addOrReplaceChild("grotto_head", CubeListBuilder.create().texOffs(0, 163).addBox(-11.0F, -19.0F, -3.0F, 22.0F, 18.0F, 3.0F, new CubeDeformation(0.0F))
 				.texOffs(177, 18).addBox(-7.0F, -7.0F, -10.0F, 14.0F, 7.0F, 10.0F, new CubeDeformation(0.0F))
